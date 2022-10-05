fe5b0b7dd60dccd9142b3853ec530f346c32de79
E-Holt <emma.d.winch@gmail.com>
2022-10-05 11:12:30 +1000

    fixed address schema mistake, final project

--- src/schemas/address_schema.py
+++ src/schemas/address_schema.py
@@ -5,7 +5,7 @@ from marshmallow import fields
 class AddressSchema(ma.Schema):
     class Meta:
         ordered = True
-        fields = ("address_id", "strees_number", "street_name", "suburb", "city", "state", "post_code")
+        fields = ("address_id", "street_number", "street_name", "suburb", "city", "state", "post_code")
 
 # Single address schema
 address_schema = AddressSchema()
